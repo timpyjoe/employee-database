fc9f39f179e0990f9ef48fdbd2b37b7fb3bf9521
Tim Poffenberger <timpoffenberger23@gmail.com>
2023-11-17 23:53:43 -0600

    nvm code is still broken thanks Gary

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function viewAllDepartments() {
     }
     console.log( `
     
-    
     `)
     console.table(results)
     start();
@@ -24,17 +23,19 @@ function viewAllRoles() {
       console.log(err);
     }
     console.table(results)
+    start();
   });
 }
 
 // Returns a table from the db of all employees
 function viewAllEmployees() {
-  db.query('SELECT * FROM employees', (err, results) => {
+  db.query('SELECT employees.id AS "Employee Id", employees.first_name AS "First Name", employees.last_name AS "Last Name", roles.title AS Position, roles.salary FROM employees INNER JOIN roles ON employees.role_id=roles.id', (err, results) => {
     if (err){
       console.log(err);
     }
     console.table(results)
-  });
+    start();
+    });
 }
 
 function addDepartment() {
@@ -53,6 +54,7 @@ function addDepartment() {
       console.log("Department successfully added!");
     }
   })
+  start();
 })
 }
 
@@ -89,6 +91,7 @@ function addRole() {
       console.log("Role successfully added!");
     }
   })
+  start();
 })
 }
 
@@ -102,7 +105,7 @@ function addEmployee() {
   })
   // prompts user for information about the new employee
   inquirer.prompt(
-    {
+    [{
       type: "input",
       message: "Please enter employee first name",
       name:  "firstName"
@@ -116,24 +119,65 @@ function addEmployee() {
       type: "list",
       message: "Which role does this employee have?",
       name: "newRole",
-      choices: currentRoles
+      choices: async function(){
+        const result = await db.promise().query("SELECT title FROM roles")
+        const data = result[0]
+        return data.map( item => ({ name: item.title, value: item.id }))
+      }
     },
     {
       type: "input",
       message: "Please enter their manager ID (leave blank for NULL)",
       name: "managerID"
-    }
+    }]
   ).then((responses) => {
     const { firstName, lastName, newRole, managerID } = responses;
+    console.log(newRole, managerID);
   //adds the new employee to the database
-  db.query("INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [firstName, lastName, (currentRoles[newRole]+1), managerID], (err, results) => {
+  db.query("INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [firstName, lastName, newRole, managerID], (err, results) => {
     if (err){
       console.log(err);
     } else {
       console.log("Employee successfully added!");
     }
   })
+  start();
 })
+}
+
+function updateEmployee() {
+  inquirer.prompt(
+    [{
+      type: "list",
+      message: "Which employee would you like to update?",
+      name: "updateEmployee",
+      choices: async function(){
+        const result = await db.promise().query("SELECT CONCAT(first_name, ' ', last_name) AS Name FROM employees")
+        const data = result[0]
+        return data.map( item => ({ name: item.Name, value: item.id }))
+      }
+    },
+    {
+      type: "list",
+      message: "Which role would you like to give them?",
+      name: "updateRole",
+      choices: async function(){
+        const result = await db.promise().query("SELECT title FROM roles")
+        const data = result[0]
+        return data.map( item => ({ name: item.title, value: item.id }))
+      }
+    }]
+  ).then((responses) => {
+    const { updateEmployee, updateRole} = responses;
+    db.query(`UPDATE employees SET role_id = ? WHERE id = ?`, [updateRole, updateEmployee], (err, results) => {
+      if (err){
+        console.log(err)
+      } else {
+        console.log("Employee successfully updated!")
+      }
+    })
+  })
+  start();
 }
 
 // starts the program
@@ -144,7 +188,7 @@ function start() {
       type: "list",
       message: "What would you like to do?",
       name: "action",
-      choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Department", "Add A Role", "Add An Employee", "Update Employee"]
+      choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Department", "Add A Role", "Add An Employee", "Update Employee Role"]
     }
   ).then((responses) => {
     //pulls the choice from "action" out as variable
@@ -163,8 +207,8 @@ function start() {
       addRole();
     } else if (action === "Add An Employee"){
       addEmployee();
-    } else if (action === "Update Employee"){
-
+    } else if (action === "Update Employee Role"){
+      updateEmployee();
     }
   })
 
